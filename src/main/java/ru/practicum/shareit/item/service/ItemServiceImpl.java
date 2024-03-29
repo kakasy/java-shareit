21567c8973e51c99be89d34cf8e8d3c687c06177
kakasy <drum2to@yandex.ru>
2024-03-30 00:40:44 +0300

    refactor: добавить зависимость хранилища пользователей для актуализации данных

--- src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -7,6 +7,7 @@ import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.mapper.ItemMapper;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.storage.ItemStorage;
+import ru.practicum.shareit.user.storage.UserStorage;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,22 +17,27 @@ import java.util.stream.Collectors;
 public class ItemServiceImpl implements ItemService {
 
     private final ItemStorage itemStorage;
+    private final UserStorage userStorage;
     private final ItemMapper itemMapper;
 
 
     @Override
     public ItemDto createItemDto(ItemDto itemDto, Long ownerId) {
 
+        userStorage.getUserById(ownerId);
         return itemMapper.toItemDto(itemStorage.createItem(itemMapper.toItem(itemDto, ownerId)));
     }
 
     @Override
     public ItemDto updateItemDto(ItemDto itemDto, Long ownerId, Long itemId) {
 
-        if (itemDto.getId() == null) {
-            itemDto.setId(itemId);
-        }
-        Item itemToUpdate = itemStorage.getItemById(itemId);
+        userStorage.getUserById(ownerId);
+
+        itemDto.setId(itemId);
+
+        Item itemToUpdate = itemStorage.getItemById(itemId)
+                .orElseThrow(() -> new EntityNotFoundException("Вещь с id=" + itemId +  " не найдена"));
+
         if (!itemToUpdate.getOwnerId().equals(ownerId)) {
             throw new EntityNotFoundException("У пользователя такой вещи не существует");
         }
@@ -42,7 +48,9 @@ public class ItemServiceImpl implements ItemService {
     @Override
     public ItemDto deleteItemDto(Long itemId, Long ownerId) {
 
-        Item itemToDelete = itemStorage.getItemById(itemId);
+        Item itemToDelete = itemStorage.getItemById(itemId)
+                .orElseThrow(() -> new EntityNotFoundException("Вещь с id=" + itemId +  " не найдена"));
+
         if (!itemToDelete.getOwnerId().equals(ownerId)) {
             throw new EntityNotFoundException("У пользователя такой вещи не существует");
         }
@@ -53,7 +61,8 @@ public class ItemServiceImpl implements ItemService {
     @Override
     public ItemDto getItemDtoById(Long itemId) {
 
-        return itemMapper.toItemDto(itemStorage.getItemById(itemId));
+        return itemMapper.toItemDto(itemStorage.getItemById(itemId)
+                .orElseThrow(() -> new EntityNotFoundException("Вещь с id= " + itemId + " не найдена")));
     }
 
     @Override
