17d075936a10bbed1d66c9b35612a179a5fd65a5
kakasy <drum2to@yandex.ru>
2024-04-20 19:31:09 +0300

    feat: обновить контроллеры

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -1,12 +1,66 @@
 package ru.practicum.shareit.booking;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.booking.dto.BookingDtoRequest;
+import ru.practicum.shareit.booking.dto.BookingDtoResponse;
 
-/**
- * TODO Sprint add-bookings.
- */
+import javax.validation.Valid;
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
+@Slf4j
 @RequestMapping(path = "/bookings")
 public class BookingController {
+
+    private static final String USER_HEADER = "X-Sharer-User-Id";
+    private final BookingService bookingService;
+
+
+    @PostMapping
+    public BookingDtoResponse create(@RequestHeader(USER_HEADER) Long userId,
+                                     @Valid @RequestBody BookingDtoRequest bookingDtoRequest) {
+
+        log.info("POST-запрос '/bookings' на создание запроса на бронирование: {} пользователем с id: {}"
+                , bookingDtoRequest, userId);
+        return bookingService.createBooking(userId, bookingDtoRequest);
+    }
+
+    @PatchMapping("/{bookingId}")
+    public BookingDtoResponse approve(@RequestHeader(USER_HEADER) Long userId, @PathVariable Long bookingId,
+                                      @RequestParam Boolean approved) {
+
+        log.info("PATCH-запрос '/bookings/{bookingId}' " +
+                "на подтверждение/отклонение бронирования: {} пользователем с id: {}", bookingId, userId);
+        return bookingService.approveBooking(userId, bookingId, approved);
+    }
+
+    @GetMapping("/{bookingId}")
+    public BookingDtoResponse getBookingById(@RequestHeader(USER_HEADER) Long userId, @PathVariable Long bookingId) {
+
+        log.info("GET-запрос '/bookings/{bookingId}' на получение данных " +
+                "о бронировании с bookingId: {} пользователем с id: {}", bookingId, userId);
+
+        return bookingService.getBookingById(userId, bookingId);
+    }
+
+    @GetMapping
+    public List<BookingDtoResponse> getBookingsByCurrentUser(
+            @RequestHeader(USER_HEADER) Long userId, @RequestParam(defaultValue = "ALL") BookingState state) {
+
+        log.info("GET-запрос '/bookings' бронирований пользователя с id:{}, state:{}", userId, state);
+
+        return bookingService.getBookingsByCurrentUser(userId, state);
+    }
+
+    @GetMapping("/owner")
+    public List<BookingDtoResponse> getBookingsFortUserItems(
+            @RequestHeader(USER_HEADER) Long ownerId, @RequestParam(defaultValue = "ALL") BookingState state) {
+
+        log.info("GET-запрос '/bookings/owner' ownerID:{}, state:{},", ownerId, state);
+
+        return bookingService.getBookingsForUserItems(ownerId, state);
+    }
 }
