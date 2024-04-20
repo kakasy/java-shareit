a5ec63679eace5078c1b1b0746a4a36f38e74255
kakasy <drum2to@yandex.ru>
2024-04-20 19:29:23 +0300

    feat: добавить dto для пользователей

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.user.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import ru.practicum.shareit.Create;
 import ru.practicum.shareit.Update;
@@ -10,6 +11,7 @@ import javax.validation.constraints.NotBlank;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class UserDto {
 
     private Long id;
